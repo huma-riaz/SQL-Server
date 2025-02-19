a8e6bd0eff6bfd27429d02b833ce2efd7ebd0143
Huma Riaz <riazhuma2005@gmail.com>
2025-02-19 22:33:11 +0500

    Create SQLQuery4.sql

---
+++ SQLQuery4.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE db_wed;
+USE db_wed;
+
+CREATE TABLE courses(
+id INT PRIMARY KEY IDENTITY,
+my_name VARCHAR (50) NOT NULL
+);
+
+CREATE TABLE student (
+id INT PRIMARY KEY IDENTITY,
+x_name VARCHAR (60),
+course INT,
+FOREIGN KEY (course) REFERENCES courses(id)
+);
+
+INSERT INTO courses VALUES ('HTML'), ('ADSE'), ('HDSE');
+
+SELECT * FROM courses;
+
+INSERT into student values('Zain',1),('Asad',2),('Omar',1);
+INSERT into student VALUES ('Bilal', 2) , ('Hassan', 3);
+
+SELECT * FROM student;
+
+DELETE FROM courses where id=4;
+
+
+
+
+
+
+
