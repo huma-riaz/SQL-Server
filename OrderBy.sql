6c66702234afacdbbf879c450c754547ea83a2a8
Huma Riaz <riazhuma2005@gmail.com>
2025-02-20 21:50:50 +0500

    Update OrderBy.sql

--- OrderBy.sql
+++ OrderBy.sql
@@ -10,4 +10,14 @@ SELECT TOP 20 * FROM std;
 
 SELECT * INTO backup_user from std;
 
+SELECT * FROM backup_user;
+
+
+SELECT myname,myemail into new_backup FROM std;
+SELECT * FROM new_backup;
+
+SELECT count(mycity) FROM std GROUP BY mycity;
+
+SELECT count(mycity) as MyCity FROM std GROUP BY mycity; 
+
 
