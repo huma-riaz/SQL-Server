1df4f8425bb7303c9b6dd2fca4c9acf794a6c588
Huma Riaz <riazhuma2005@gmail.com>
2025-02-27 23:38:20 +0500

    Update Trigger.sql

--- Trigger.sql
+++ Trigger.sql
@@ -2,4 +2,12 @@ CREATE TRIGGER after_insert
 on myindex
 after insert 
 AS
-declare @myid int,
+declare @myid int;
+declare @myname varchar(50);
+declare @myemail varchar(40);
+declare @myage int;
+
+select @myid = id from inserted;
+select @myid = id from inserted;
+
+
